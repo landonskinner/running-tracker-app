687409586f80e6d0317ea6da8815b19cfad4e7dc
Landon Skinner <landonjskinner@gmail.com>
2022-04-04 15:23:34 -0500

    accounted for edge case of no scheduled runs in stats calcs

--- components/RunStats.js
+++ components/RunStats.js
@@ -2,10 +2,11 @@ function RunStats({user}) {
 
     const {id, firstName, lastName, scheduledRuns} = user;
 
-    const dateLimit = (length) => {
+    const dateLimit = (period) => {
         const date = new Date()
-        if (length === 'week') date.setDate(date.getDate() - 7)
-        if (length === 'month') date.setMonth(date.getMonth() - 1)
+        if (period === 'week') date.setDate(date.getDate() - 7)
+        if (period === 'month') date.setMonth(date.getMonth() - 1)
+        if (period === 'all') return date.toISOString
         date.setHours(0)
         date.setMinutes(0)
         date.setSeconds(0)
@@ -13,23 +14,28 @@ function RunStats({user}) {
         return date.toISOString()
     }
 
-    const filteredRuns = (length) => scheduledRuns.filter(run => {
+    const filteredRuns = (period) => scheduledRuns.filter(run => {
         // filter runs that have been completed within the specified timeframe
-        return run.completedRun && run.date >= dateLimit(length)
+        if (period === 'all') return run.completedRun
+        return run.completedRun && run.date >= dateLimit(period)
     })
 
     // initialize arrays of objects containing runs within specified timeframe
     const weekRuns = filteredRuns('week');
     const monthRuns = filteredRuns('month');
-    const allRuns = scheduledRuns.filter(run => run.completedRun);
+    const allRuns = filteredRuns('all');
+
+    const noDataMsg = '---'
 
     const averageLength = (runsArr) => {
+        if (!runsArr.length) return noDataMsg
         let totalDistance = 0;
         runsArr.forEach(run => totalDistance += run.completedRun.realDistance)
         return totalDistance/runsArr.length
     }
 
     const averagePace = (runsArr) => {
+        if (!runsArr.length) return noDataMsg
         let totalMinutes = 0;
         let totalSeconds = 0;
         runsArr.forEach(run => {
@@ -43,25 +49,31 @@ function RunStats({user}) {
     }
 
     const averageCalories = (runsArr) => {
+        if (!runsArr.length) return noDataMsg
         let totalCalories = 0;
         runsArr.forEach(run => totalCalories += run.completedRun.calories)
         return totalCalories/runsArr.length
     }
 
     const averageRating = (runsArr) => {
+        if (!runsArr.length) return noDataMsg
         let totalRating = 0;
         runsArr.forEach(run => totalRating += run.completedRun.rating)
         return totalRating/runsArr.length
     }
 
-    const percentCompleted = (runsArr, length) => {
-        const totalScheduled = scheduledRuns.filter(run => run.date >= dateLimit(length)).length
-        return Math.round(runsArr.length/totalScheduled * 100)
+    const percentCompleted = (runsArr, period) => {
+        // if (!runsArr.length) return noDataMsg
+        let totalScheduled;
+        if (period === 'all') {
+            totalScheduled = scheduledRuns.filter(run => run.date < new Date().toISOString()).length
+        } else {
+            totalScheduled = scheduledRuns.filter(run => run.date >= dateLimit(period)).length
+        }
+        if (!totalScheduled) return noDataMsg
+        return Math.round(runsArr.length/totalScheduled * 100) + '%'
     }
 
-console.log(averageRating(monthRuns))
-    // console.log(averae)
-    // 604800000
     return (
         <div>
             <ul>
@@ -77,9 +89,9 @@ console.log(averageRating(monthRuns))
                 <li>Average Run Rating (Week): {averageRating(weekRuns)}</li>
                 <li>Average Run Rating (Month): {averageRating(monthRuns)}</li>
                 <li>Average Run Rating (All Time): {averageRating(allRuns)}</li>
-                <li>Percent of Runs Completed (Week): {percentCompleted(weekRuns, 'week')}</li>
-                <li>Percent of Runs Completed (Month): {percentCompleted(monthRuns, 'month')}</li>
-                <li>Percent of Runs Completed (All Time): {Math.round(allRuns.length/scheduledRuns.length * 100)}</li>
+                <li>Percent of Scheduled Runs Completed (Week): {percentCompleted(weekRuns, 'week')}</li>
+                <li>Percent of Scheduled Runs Completed (Month): {percentCompleted(monthRuns, 'month')}</li>
+                <li>Percent of Scheduled Runs Completed (All Time): {percentCompleted(allRuns, 'all')}</li>
             </ul>
         </div>
     )
